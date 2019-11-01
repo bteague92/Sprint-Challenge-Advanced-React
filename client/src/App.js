8cae288029120bd73fd6268bf5305dee7d4f2220
[Brandon Teague] <[teaguebrandon@outlook.com]>
2019-11-01 11:13:51 -0400

    set up file structure and axios call

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from "axios";
+import PropTypes from 'prop-types';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    data: [],
+  };
+
+  componentDidMount() {
+    axios
+      .get(`http://localhost:5000/api/players`)
+      .then(res => {
+        console.log("this is res", res);
+        this.setState({
+          data: res.data
+        })
+        console.log(this.state.data)
+      })
+      .catch(err => console.log(err));
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+
+  }
+
+  componentWillUnmount() {
+
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Working!</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
